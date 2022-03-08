081e71e09e874d138dd67464beda1005a4c759f5
mazzah0316 <mazzah@oregonstate.edu>
2022-03-08 14:48:13 -0800

    Update HM_bar_charts.svelte

--- src/components/HM_bar_charts.svelte
+++ src/components/HM_bar_charts.svelte
@@ -1,13 +1,143 @@
-<script>
+ <script>
+    import { addedTracks, seededTracks, recommendedTracks } from '../stores.js';
+    import { onMount } from "svelte";
+    import { scaleLinear } from 'd3-scale';
 
+    let addTracks;
+    let graphedTracksArtists;
+
+    addedTracks.subscribe((v) => {
+        addTracks = v;
+        graphedTracksArtists = findOcc(addTracks,"artist");
+        console.log(graphedTracksArtists);
+  });   
+
+    function findOcc(arr, key){
+    let arr2 = [];
+	
+        arr.forEach((x)=>{
+            if(arr2.some((val)=>{ return val[key] == x[key] })){
+                arr2.forEach((k)=>{
+                    if(k[key] === x[key]){
+                    k["occurrence"]++
+                    }
+                })
+                }else{
+                let a = {}
+                a[key] = x[key]
+                a["occurrence"] = 1
+                arr2.push(a);
+                }
+            })
+            
+            return arr2
+    }
+
+	const xTicks = [1990, 1995, 2000, 2005, 2010, 2015];
+	const yTicks = [0, 0.05, .10, .15, .20];
+	const padding = { top: 20, right: 15, bottom: 20, left: 25 };
+
+	let width = 500;
+	let height = 200;
+
+	$: xScale = scaleLinear()
+		.domain([0, xTicks.length])
+		.range([padding.left, width - padding.right]);
+
+	$: yScale = scaleLinear()
+		.domain([0, Math.max.apply(null, yTicks)])
+		.range([height - padding.bottom, padding.top]);
+
+	$: innerWidth = width - (padding.left + padding.right);
+	$: barWidth = innerWidth / xTicks.length;
 </script>
 
-<div id="BarCharts">
-    <p>3 Bar Charts</p>
+<div class="container">
+    <div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
+        <svg>
+            <!-- y axis -->
+            <g class="axis y-axis">
+                {#each yTicks as tick}
+                    <g class="tick tick-{tick}" transform="translate(0, {yScale(tick)})">
+                        <line x2="100%"></line>
+                        <text y="-4">{tick} {tick === 20 ? ' per 1,000 population' : ''}</text>
+                    </g>
+                {/each}
+            </g>
+
+            <!-- x axis -->
+            <g class="axis x-axis">
+                {#each graphedTracksArtists as point, i}
+                    <g class="tick" transform="translate({xScale(i)},{height})">
+                        <text x="{barWidth/2}" y="-4">{width > 380 ? point.artist : formatMobile(point.artist)}</text>
+                    </g>
+                {/each}
+            </g>
+
+            <g class='bars'>
+                {#each graphedTracksArtists as point, i}
+                    <rect
+                        x="{xScale(i) + 2}"
+                        y="{yScale(point.occurance)}"
+                        width="{barWidth - 4}"
+                        height="{yScale(point.occurance)}"
+                    ></rect>
+                {/each}
+            </g>
+        </svg>
+    </div>
 </div>
 
+
 <style>
-#BarCharts{
-    height: 100%;
-}
+    #BarCharts{
+        height: 100%;
+    }
+    .container {
+    display: flex;
+    width: 100%;
+    height: 150px;
+    }
+
+    .chart {
+		width: 100%;
+		max-width: 500px;
+		margin: 0 auto;
+	}
+
+	svg {
+		position: relative;
+		width: 100%;
+		height: 200px;
+	}
+
+	.tick {
+		font-family: Helvetica, Arial;
+		font-size: .725em;
+		font-weight: 200;
+	}
+
+	.tick line {
+		stroke: #e2e2e2;
+		stroke-dasharray: 2;
+	}
+
+	.tick text {
+		fill: #ccc;
+		text-anchor: start;
+	}
+
+	.tick.tick-0 line {
+		stroke-dasharray: 0;
+	}
+
+	.x-axis .tick text {
+		text-anchor: middle;
+	}
+
+	.bars rect {
+		fill: #a11;
+		stroke: none;
+		opacity: 0.65;
+	}
 </style>
